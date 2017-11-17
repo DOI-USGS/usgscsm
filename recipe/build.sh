e89a43b7fd7d3eb534934bf2cf4509d1e8d7f306
jay <jlaura@asu.edu>
2017-11-17 14:47:40 -0700

    Differentiate OSX / Linux for conda

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 
 if [ `uname` == Darwin ]; then
   export MACOSX_DEPLOYMENT_TARGET=10.9;
+  $PYTHON setup.py install;
+else
+  CC=gcc-6 CXX=g++-6 $PYTHON setup.py install;
 fi
-
-CC=gcc-6 CXX=g++-6 $PYTHON setup.py install
