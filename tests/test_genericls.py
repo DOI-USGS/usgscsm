a1ae5240c06cf2652f6318a46ebafbf406f95389
Jesse Mapel <jmapel@usgs.gov>
2018-02-27 16:44:47 -0700

    Added HRSC ground to image tests

--- tests/test_genericls.py
+++ tests/test_genericls.py
@@ -35,27 +35,32 @@ class TestCTX:
     #    assert y == pytest.approx(iy)
 
 class TestHRSC:
+    #TODO make the test tolerances tighter
 
     @pytest.mark.parametrize('image, ground',[
-                              ((14351.5, 1291.5, 0), (1050372.2680538, 995148.04296401, -3054455.4812471))
+                              ((14351.5, 1291.5, 0), (1050372.2680538, 995148.04296401, -3054455.4812471)),
+                              ((0.5, 0.5, 0), (707400.76484944, 670982.96805253, -3234076.4365304)),
+                              ((0.5, 2583.5, 0), (788620.52258149, 602466.17532249, -3228838.5855823)),
+                              ((28703.5, 0.5, 0), (1263308.4518651, 1299579.9304643, -2855245.3087177)),
+                              ((28703.5, 2583.5, 0), (1317920.8068939, 1251608.9818902, -2852027.3218065))
     ])
     def test_image_to_ground(self, hrsc_model, image, ground):
         gx, gy, gz = ground
         x, y, z = hrsc_model.imageToGround(*image)
-        #TODO: Get this test up and running.
-        #print(x, y, z)
-        #assert False
         assert x == pytest.approx(gx, rel=1e-3)
         assert y == pytest.approx(gy, rel=1e-3)
         assert z == pytest.approx(gz, rel=1e-3)
 
-    #@pytest.mark.parametrize('image, ground',[
-    #                          ((512, 512, 0), (-73589.5516508502, 562548.342040933, 2372508.44060771)),
-    #                          ((100, 100, 0), (-48020.2164819883, 539322.805489926, 2378549.41724731))
-    #])
-    #def test_ground_to_image(self, model, image, ground):
-    #    y, x = model.groundToImage(*ground)
-    #    ix, iy, _ = image
-#
-    #    assert x == pytest.approx(ix)
-    #    assert y == pytest.approx(iy)
+    @pytest.mark.parametrize('image, ground',[
+                              ((14351.5, 1291.5, 0), (1050372.2680538, 995148.04296401, -3054455.4812471)),
+                              ((0.5, 0.5, 0), (707400.76484944, 670982.96805253, -3234076.4365304)),
+                              ((0.5, 2583.5, 0), (788620.52258149, 602466.17532249, -3228838.5855823)),
+                              ((28703.5, 0.5, 0), (1263308.4518651, 1299579.9304643, -2855245.3087177)),
+                              ((28703.5, 2583.5, 0), (1317920.8068939, 1251608.9818902, -2852027.3218065))
+    ])
+    def test_ground_to_image(self, hrsc_model, image, ground):
+        y, x = hrsc_model.groundToImage(*ground)
+        iy, ix, _ = image
+
+        assert x == pytest.approx(ix, abs=0.5)
+        assert y == pytest.approx(iy, abs=0.5)
