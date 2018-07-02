e00d6aea30013acc529ffedbc4953c89c104ad80
jlaura <jlaura@usgs.gov>
2018-07-02 07:14:41 -0700

    Adds __version__ attribute (#35)
    * Refactors out bad class attribute
    
    * Adds dynamic versioning

--- usgscam/__init__.py
+++ usgscam/__init__.py
@@ -1,5 +1,20 @@
+
+from pkg_resources import get_distribution, DistributionNotFound
+import os.path
+
+try:
+    _dist = get_distribution('autocnet')
+    # Normalize case for Windows systems
+    dist_loc = os.path.normcase(_dist.location)
+    here = os.path.normcase(__file__)
+    if not here.startswith(os.path.join(dist_loc, 'autocnet')):
+        # not installed, but there is another version that *is*
+        raise DistributionNotFound
+except DistributionNotFound:
+    __version__ = 'Please install this project with setup.py'
+else:
+    __version__ = _dist.version
+
 import usgscam.genericframe
-import usgscam.orex
 import usgscam.genericls
-#from usgscam import mdis
-#from usgscam import orex
+import usgscam.orex
