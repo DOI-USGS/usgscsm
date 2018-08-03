f7c9e4ffaacf254aae6bd8fb0e2e9cc72a9efa58
Makayla Shepherd <mshepherd@usgs.gov>
2018-08-03 15:55:57 -0700

    Added a check to see if gtest has already been added as a library.

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -1,17 +1,19 @@
-set(GOOGLETEST_ROOT gtest/googletest CACHE STRING "Google Test source root")
+if (NOT TARGET gtest)
+  set(GOOGLETEST_ROOT gtest/googletest CACHE STRING "Google Test source root")
 
-include_directories(SYSTEM
-    ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}
-    ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/include
-    )
+  include_directories(SYSTEM
+      ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}
+      ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/include
+      )
 
-set(GOOGLETEST_SOURCES
-    ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest-all.cc
-    ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest_main.cc
-    )
+  set(GOOGLETEST_SOURCES
+      ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest-all.cc
+      ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest_main.cc
+      )
 
-foreach(_source ${GOOGLETEST_SOURCES})
-    set_source_files_properties(${_source} PROPERTIES GENERATED 1)
-endforeach()
+  foreach(_source ${GOOGLETEST_SOURCES})
+      set_source_files_properties(${_source} PROPERTIES GENERATED 1)
+  endforeach()
 
-add_library(gtest ${GOOGLETEST_SOURCES})
+  add_library(gtest ${GOOGLETEST_SOURCES})
+endif()
