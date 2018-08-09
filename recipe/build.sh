6af4ca51e1f0248c781266779a154df89470a147
Jay <jlaura@usgs.gov>
2018-08-09 11:12:11 -0700

    Updates CMake builds to optionally build CSM

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+source activate base
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_CSM=OFF ..
 make install
