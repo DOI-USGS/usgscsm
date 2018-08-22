d6ad494a2dffd7655ab52e5bf07a3bc4afa1105b
Tyler Wilson <twilson271828@gmail.com>
2018-08-22 14:53:20 -0700

    Refactored setFocalPlane test so that the ISD changes are made local to the test (and not global).

--- tests/FrameCameraTests.cpp
+++ tests/FrameCameraTests.cpp
@@ -23,35 +23,33 @@ class FrameSensorModel : public testing::Test {
 
       protected :
       UsgsAstroFrameSensorModel *sensorModel;
+      void loadIsd(const std::string pathtoIsd,csm::Isd &isd){
+        std::ifstream isdFile("data/simpleFramerISD.json");
+        json jsonIsd = json::parse(isdFile);
+
+        for (json::iterator it = jsonIsd.begin(); it != jsonIsd.end(); ++it) {
+           json jsonValue = it.value();
+           if (jsonValue.size() > 1) {
+             for (int i = 0; i < jsonValue.size(); i++) {
+                   isd.addParam(it.key(), jsonValue[i].dump());
+             }
+           }
+           else {
+              isd.addParam(it.key(), jsonValue.dump());
+           }
+        }
+      }
 
 
       void SetUp() override {
          sensorModel = NULL;
-
-
-          vector<double> odtx{0.0,1.0020558791381275,0.0,0.0,-0.0005448742222712919,0.0,0.000006597498811862692,0.0,0.000006683129056014682,0.0};
-          vector<double> odty{-0.000013657975359540047,0.0,1.0,0.000885544334965699,0.0,0.0003338939138331483,0.0,0.00000774756721313425,0.0,0.00000779484564042716};
-
          std::ifstream isdFile("data/simpleFramerISD.json");
-
          json jsonIsd = json::parse(isdFile);
-
          csm::Isd isd;
          for (json::iterator it = jsonIsd.begin(); it != jsonIsd.end(); ++it) {
             json jsonValue = it.value();
             if (jsonValue.size() > 1) {
-               for (int i = 0; i < jsonValue.size(); i++) {                              
-                 if (it.key() == "odt_x") {
-                    ostringstream strval;
-                    strval << setprecision(20) << odtx[i];
-                    isd.addParam(it.key(), strval.str());
-                  }
-                  else if(it.key() == "odt_y") {
-                   ostringstream strval;
-                   strval << setprecision(20) << odty[i];
-                   isd.addParam(it.key(), strval.str());
-                  }
-                  else
+              for (int i = 0; i < jsonValue.size(); i++){
                     isd.addParam(it.key(), jsonValue[i].dump());
                }
             }
@@ -131,13 +129,32 @@ TEST_F(FrameSensorModel, OffBody4) {
    EXPECT_NEAR(groundPt.z, -14.99325304, 1e-8);
 }
 
-TEST_F(FrameSensorModel, setFocalPlane) {
+TEST_F(FrameSensorModel, setFocalPlane1) {
+  int precision  = 20;
 
-   csm::ImageCoord imagePt(6.99224757243428, 7.065069863824555);
+   csm::Isd localIsd;
+   loadIsd("data/simpleFramerISD.json",localIsd);
+   csm::ImageCoord imagePt(7.0, 7.0);
    double ux,uy;
-    sensorModel->setFocalPlane(imagePt.samp, imagePt.line, ux, uy);
-   EXPECT_NEAR(ux,7.0,1e-2 );
-   EXPECT_NEAR(uy,7.0,1e-2);
+   vector<double> odtx{0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+   vector<double> odty{0.0,1.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+
+   localIsd.clearParams("odt_x");
+   localIsd.clearParams("odt_y");
+   for (auto & val: odtx){
+      ostringstream strval;
+      strval << setprecision(precision) << val;
+      localIsd.addParam("odt_x", strval.str());
+   }
+   for (auto & val: odty){
+      ostringstream strval;
+      strval << setprecision(precision) << val;
+      localIsd.addParam("odt_y", strval.str());
+   }
+
+   sensorModel->setFocalPlane(imagePt.samp, imagePt.line, ux, uy);
+   EXPECT_NEAR(ux,7.0,1e-8 );
+   EXPECT_NEAR(uy,7.0,1e-8);
 
 }
 
