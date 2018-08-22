05ca11825ebaad00668e29af5116a3effd8b610e
Kaj Williams <kewilliams@usgs.gov>
2018-08-22 13:08:29 -0700

    fixed isd variable again

--- tests/FrameCameraTests.cpp
+++ tests/FrameCameraTests.cpp
@@ -11,6 +11,7 @@ using json = nlohmann::json;
 class FrameIsdTest : public ::testing::Test {
    protected:
 
+      csm::Isd isd;
 
    virtual void SetUp() {
       std::ifstream isdFile("data/simpleFramerISD.json");
@@ -34,7 +35,7 @@ class FrameSensorModel : public ::testing::Test {
    protected:
 
       UsgsAstroFrameSensorModel *sensorModel;
-      csm::Isd isd;
+      
       void SetUp() override {
          sensorModel = NULL;
          std::ifstream isdFile("data/simpleFramerISD.json");
