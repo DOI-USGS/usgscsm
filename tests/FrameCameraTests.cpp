7a565afa248248ffe36b8f152b6730fb2552a680
Jesse Mapel <jmapel@usgs.gov>
2018-10-02 13:48:29 -0700

    Fixed optical distortion and pixel to focal transforms.

--- tests/FrameCameraTests.cpp
+++ tests/FrameCameraTests.cpp
@@ -13,7 +13,7 @@
 
 //centered and slightly off-center:
 TEST_F(FrameSensorModel, Center) {
-   csm::ImageCoord imagePt(7.0, 7.0);
+   csm::ImageCoord imagePt(7.5, 7.5);
    csm::EcefCoord groundPt = sensorModel->imageToGround(imagePt, 0.0);
    EXPECT_NEAR(groundPt.x, 10.0, 1e-8);
    EXPECT_NEAR(groundPt.y, 0, 1e-8);
@@ -21,7 +21,7 @@ TEST_F(FrameSensorModel, Center) {
 }
 
 TEST_F(FrameSensorModel, SlightlyOffCenter) {
-   csm::ImageCoord imagePt(7.0, 6.0);
+   csm::ImageCoord imagePt(7.5, 6.5);
    csm::EcefCoord groundPt = sensorModel->imageToGround(imagePt, 0.0);
    EXPECT_NEAR(groundPt.x, 9.80194018, 1e-8);
    EXPECT_NEAR(groundPt.y, 0, 1e-8);
@@ -30,28 +30,28 @@ TEST_F(FrameSensorModel, SlightlyOffCenter) {
 
 //Test all four corners:
 TEST_F(FrameSensorModel, OffBody1) {
-   csm::ImageCoord imagePt(14.5, -0.5);
+   csm::ImageCoord imagePt(15.0, 0.0);
    csm::EcefCoord groundPt = sensorModel->imageToGround(imagePt, 0.0);
    EXPECT_NEAR(groundPt.x, 0.44979759, 1e-8);
    EXPECT_NEAR(groundPt.y, -14.99325304, 1e-8);
    EXPECT_NEAR(groundPt.z, 14.99325304, 1e-8);
 }
 TEST_F(FrameSensorModel, OffBody2) {
-   csm::ImageCoord imagePt(-0.5, 14.5);
+   csm::ImageCoord imagePt(0.0, 15.0);
    csm::EcefCoord groundPt = sensorModel->imageToGround(imagePt, 0.0);
    EXPECT_NEAR(groundPt.x, 0.44979759, 1e-8);
    EXPECT_NEAR(groundPt.y, 14.99325304, 1e-8);
    EXPECT_NEAR(groundPt.z, -14.99325304, 1e-8);
 }
 TEST_F(FrameSensorModel, OffBody3) {
-   csm::ImageCoord imagePt(-0.5, -0.5);
+   csm::ImageCoord imagePt(0.0, 0.0);
    csm::EcefCoord groundPt = sensorModel->imageToGround(imagePt, 0.0);
    EXPECT_NEAR(groundPt.x, 0.44979759, 1e-8);
    EXPECT_NEAR(groundPt.y, 14.99325304, 1e-8);
    EXPECT_NEAR(groundPt.z, 14.99325304, 1e-8);
 }
 TEST_F(FrameSensorModel, OffBody4) {
-   csm::ImageCoord imagePt(14.5, 14.5);
+   csm::ImageCoord imagePt(15.0, 15.0);
    csm::EcefCoord groundPt = sensorModel->imageToGround(imagePt, 0.0);
    EXPECT_NEAR(groundPt.x, 0.44979759, 1e-8);
    EXPECT_NEAR(groundPt.y, -14.99325304, 1e-8);
