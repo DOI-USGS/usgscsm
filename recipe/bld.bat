e086dc97ad6f2b1d32753cafff60cdd7ad1cc6d6
jlaura <jlaura@usgs.gov>
2018-10-18 19:46:14 -0700

    Update bld.bat
    This is a test to see where appveyor is failing.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,4 +3,4 @@ cd build
 cmake -G "Visual Studio 15 2017 Win64" -DBUILD_TESTS=OFF ..
 cmake --build . --target ALL_BUILD --config Release
 copy Release\usgscsm.dll %LIBRARY_BIN%
-copy Release\usgscsm.lib %LIBRARY_LIB%
+copy Release\usgscsm.lib %LIBRARY_BIN%
