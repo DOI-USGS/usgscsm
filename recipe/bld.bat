c9a7d17de191bde1f674a88823af886d9a8fc2d4
jlaura <jlaura@usgs.gov>
2018-10-18 19:48:57 -0700

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,3 +4,5 @@ cmake -G "Visual Studio 15 2017 Win64" -DBUILD_TESTS=OFF ..
 cmake --build . --target ALL_BUILD --config Release
 copy Release\usgscsm.dll %LIBRARY_BIN%
 copy Release\usgscsm.lib %LIBRARY_BIN%
+
+if errorlevel 1 exit 1 
