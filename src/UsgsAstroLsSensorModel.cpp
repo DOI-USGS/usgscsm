7079a414ded88052f4e27d9c2d4c3247825a814e
Jesse Mapel <jam826@nau.edu>
2019-02-19 17:01:04 -0700

    Fixed state being written out and read in differently (#183)
    * Fixed state being written out and read in differently
    
    * Fixed canModelBeConstructedFromState

--- src/UsgsAstroLsSensorModel.cpp
+++ src/UsgsAstroLsSensorModel.cpp
@@ -252,6 +252,7 @@ std::string UsgsAstroLsSensorModel::getModelNameFromModelState(
 
 std::string UsgsAstroLsSensorModel::getModelState() const {
       json state;
+      state["m_modelName"] = _SENSOR_MODEL_NAME;
       state["m_imageIdentifier"] = m_imageIdentifier;
       state["m_sensorType"] = m_sensorType;
       state["m_totalLines"] = m_totalLines;
@@ -306,9 +307,9 @@ std::string UsgsAstroLsSensorModel::getModelState() const {
       state["m_imageFlipFlag"] = m_imageFlipFlag;
 
       state["m_referencePointXyz"] = json();
-      state["m_referencePointXyz"]["x"] = m_referencePointXyz.x;
-      state["m_referencePointXyz"]["y"] = m_referencePointXyz.y;
-      state["m_referencePointXyz"]["z"] = m_referencePointXyz.z;
+      state["m_referencePointXyz"][0] = m_referencePointXyz.x;
+      state["m_referencePointXyz"][1] = m_referencePointXyz.y;
+      state["m_referencePointXyz"][2] = m_referencePointXyz.z;
 
       return state.dump();
  }
