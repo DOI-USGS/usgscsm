f3e33bac04dd306028b78ab35965b97f56080b52
Jesse Mapel <jmapel@usgs.gov>
2020-04-16 06:52:35 -0700

    Added root finding algorithm (#277)

--- include/usgscsm/Utilities.h
+++ include/usgscsm/Utilities.h
@@ -69,6 +69,14 @@ void lagrangeInterp (
   const int&     i_order,
   double*        valueVector);
 
+// Brent's algorithm for finding the roots of a function
+// Arguments are two inputs that bracket a root, the function, and a convergence tolerance
+double brentRoot(
+  double lowerBound,
+  double upperBound,
+  double (*func)(double),
+  double epsilon = 1e-10);
+
 // Methods for checking/accessing the ISD
 
 double metric_conversion(double val, std::string from, std::string to="m");
