d0731dfd9cf1b87ee0067b378a9c3825a6d75a9e
Adam Paquette <acpaquette@usgs.gov>
2020-06-24 12:27:38 -0700

    Set default logger for framer

--- include/usgscsm/UsgsAstroFrameSensorModel.h
+++ include/usgscsm/UsgsAstroFrameSensorModel.h
@@ -384,7 +384,7 @@ class UsgsAstroFrameSensorModel : public csm::RasterGM, virtual public csm::Sett
 
     csm::EcefCoord m_referencePointXyz;
 
-    std::string m_logName;
+    std::string m_logName = "";
 
     json _state;
     static const int         _NUM_STATE_KEYWORDS;
