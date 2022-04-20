c3503fec05bceed4379b282d5b530776216f40b5
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-04-20 16:09:44 -0700

    usgscsm_cam_test: Finish loading functionality

--- bin/usgscsm_cam_test.cc
+++ bin/usgscsm_cam_test.cc
@@ -1,16 +1,56 @@
 // A tool to perform some basic tests and operations on a CSM camera model.
+// 
 // Functionality:
+//
+// - Load a CSM model in ISD format.
+//
+// Future functionality:
 // 
 // - Test projecting rays from the camera to ground and vice-versa.
+// - Load a CSM model state (stored in a .json file, just like
+//   an ISD model).
+// - Ability to export a CSM model in ISD format to a CSM model state file. 
 
 #include <usgscsm/UsgsAstroPlugin.h>
-#include <usgscsm/UsgsAstroLsSensorModel.h>
-#include <usgscsm/UsgsAstroFrameSensorModel.h>
-#include <usgscsm/UsgsAstroSarSensorModel.h>
+#include <csm/RasterGM.h>
 
 #include <iostream>
 int main(int argc, char **argv) {
-  std::cout << "--test is xxx"  << std::endl;
+
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <model file>" << std::endl;
+    return 1;
+  }
+  
+  std::string model_file = argv[1];
+  csm::Isd isd(model_file);
+
+  std::vector<std::string> model_types = {"USGS_ASTRO_FRAME_SENSOR_MODEL",
+                                          "USGS_ASTRO_LINE_SCANNER_SENSOR_MODEL",
+                                          "USGS_ASTRO_SAR_SENSOR_MODEL"};
+
+  // Try to load the model
+  UsgsAstroPlugin plugin;
+  std::shared_ptr<csm::RasterGM> model;
+  csm::WarningList* warnings = NULL;
+  for (size_t it = 0; it < model_types.size(); it++) {
+    if (plugin.canModelBeConstructedFromISD(isd, model_types[it])) {
+
+      csm::Model *csm = plugin.constructModelFromISD(isd, model_types[it], warnings);
+      csm::RasterGM *modelPtr = dynamic_cast<csm::RasterGM*>(csm);
+
+      if (modelPtr == NULL) {
+        std::cerr << "Could not load correctly a CSM model of type: "
+                  << model_types[it] << std::endl;
+      } else {
+        // Assign it to a smart pointer which will handle its deallocation
+        model = std::shared_ptr<csm::RasterGM>(modelPtr);
+        std::cout << "Loaded CSM model of type " << model_types[it]
+                  << " from " << model_file << ".\n";
+      }
+    }
+  }
+
   return 0;
 }
 
