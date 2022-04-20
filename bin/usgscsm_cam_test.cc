8802a76399d97b46733d5f69f7cefed7e20600c3
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-04-20 14:51:00 -0700

    Flesh out a bit the proposed test tool

---
+++ bin/usgscsm_cam_test.cc
@@ -0,0 +1,16 @@
+// A tool to perform some basic tests and operations on a CSM camera model.
+// Functionality:
+// 
+// - Test projecting rays from the camera to ground and vice-versa.
+
+#include <usgscsm/UsgsAstroPlugin.h>
+#include <usgscsm/UsgsAstroLsSensorModel.h>
+#include <usgscsm/UsgsAstroFrameSensorModel.h>
+#include <usgscsm/UsgsAstroSarSensorModel.h>
+
+#include <iostream>
+int main(int argc, char **argv) {
+  std::cout << "--test is xxx"  << std::endl;
+  return 0;
+}
+
