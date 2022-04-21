5cae8486a74296e4ac1a5b2242039032eb0ea01f
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2022-04-21 09:41:29 -0700

    usgscsm_cam_test: Use the more generic CSM approach

--- bin/usgscsm_cam_test.cc
+++ bin/usgscsm_cam_test.cc
@@ -13,6 +13,7 @@
 
 #include <usgscsm/UsgsAstroPlugin.h>
 #include <csm/RasterGM.h>
+#include <UsgsAstroLsSensorModel.h>
 
 #include <iostream>
 int main(int argc, char **argv) {
@@ -21,39 +22,55 @@ int main(int argc, char **argv) {
     std::cerr << "Usage: " << argv[0] << " <model file>" << std::endl;
     return 1;
   }
-  
+
+  // This is needed to trigger loading libusgscsm.so. Otherwise 0
+  // plugins are detected.
+  UsgsAstroLsSensorModel lsModel;
+
+  // Load the isd
   std::string model_file = argv[1];
   csm::Isd isd(model_file);
-
-  std::vector<std::string> model_types = {"USGS_ASTRO_FRAME_SENSOR_MODEL",
-                                          "USGS_ASTRO_LINE_SCANNER_SENSOR_MODEL",
-                                          "USGS_ASTRO_SAR_SENSOR_MODEL"};
-
-  // Try to load the model
+  
+  // Check if loading the model worked
   bool success = false;
-  UsgsAstroPlugin plugin;
-  std::shared_ptr<csm::RasterGM> model;
-  csm::WarningList* warnings = NULL;
-  for (size_t it = 0; it < model_types.size(); it++) {
-    if (plugin.canModelBeConstructedFromISD(isd, model_types[it])) {
 
-      csm::Model *csm = plugin.constructModelFromISD(isd, model_types[it], warnings);
-      csm::RasterGM *modelPtr = dynamic_cast<csm::RasterGM*>(csm);
+  // Try all detected plugins and models for each plugin
+  csm::PluginList plugins = csm::Plugin::getList();
+  for (auto iter = plugins.begin(); iter != plugins.end(); iter++) {
+    
+    const csm::Plugin* csm_plugin = (*iter);
 
-      if (modelPtr == NULL) {
-        std::cerr << "Could not load correctly a CSM model of type: "
-                  << model_types[it] << std::endl;
-        return 1;
-      } else {
-        // Assign it to a smart pointer which will handle its deallocation
-        model = std::shared_ptr<csm::RasterGM>(modelPtr);
-        success = true;
-        std::cout << "Loaded CSM model of type " << model_types[it]
-                  << " from " << model_file << ".\n";
+    std::cout << "Detected CSM plugin: " << csm_plugin->getPluginName()  << "\n";
+    // For each plugin, loop through the available models.
+    size_t num_models = csm_plugin->getNumModels();
+    std::cout << "Number of models for this plugin: " << num_models << "\n";
+    for (size_t i = 0; i < num_models; i++) {
+
+      std::string model_name = (*iter)->getModelName(i);
+
+      std::shared_ptr<csm::RasterGM> model;
+      csm::WarningList* warnings = NULL;
+
+      if (csm_plugin->canModelBeConstructedFromISD(isd, model_name)) {
+
+        csm::Model *csm = csm_plugin->constructModelFromISD(isd, model_name, warnings);
+        csm::RasterGM *modelPtr = dynamic_cast<csm::RasterGM*>(csm);
+        
+        if (modelPtr == NULL) {
+          std::cerr << "Could not load correctly a CSM model of type: "
+                    << model_name << "\n";
+          return 1;
+        } else {
+          // Assign it to a smart pointer which will handle its deallocation
+          model = std::shared_ptr<csm::RasterGM>(modelPtr);
+          success = true;
+          std::cout << "Loaded CSM model of type " << model_name
+                    << " from " << model_file << ".\n";
+        }
       }
     }
   }
-
+  
   if (!success) {
     std::cerr << "Failed to load a CSM model from: " << model_file << ".\n";
     return 1;
@@ -61,4 +78,3 @@ int main(int argc, char **argv) {
 
   return 0;
 }
-
