d6c63bb046a0b14f6f54fc995687f563d346f124
acpaquette <acpaquette@usgs.gov>
2023-08-07 19:20:56 -0700

    Updated build flags for future releases

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTS=OFF ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DUSGSCSM_BUILD_TESTS=OFF ..
 make install
