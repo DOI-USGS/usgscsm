d1ae2102ba42b877095bef94439d1a48070e6e56
acpaquette <acpaquette@usgs.gov>
2023-08-31 17:37:18 -0700

    Finished OCAP header addition

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,16 +48,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-html_js_files = ['usgsbanner.js']
-
-html_css_files = [
-    '_static/common.css',
-    '_static/custom.css',
-    '_static/uswds_header.css'
-]
-
+# html_static_path = ['_static']
 
 # breathe (doxygen -> sphinx package) settings
 breathe_projects = { "USGSCSM": "../xml/" }
