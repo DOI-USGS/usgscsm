62aba19f5e3ff0ac5e91d587584286fdd108f57b
acpaquette <acpaquette@usgs.gov>
2024-01-18 13:17:15 -0700

    Small changes to get the build working

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+git submodule update --init
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DUSGSCSM_EXTERNAL_DEPS=OFF -DUSGSCSM_BUILD_DOCS=OFF -DUSGSCSM_BUILD_TESTS=OFF ..
 make install
